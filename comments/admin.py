b3c7de2a7919267651c895f26b64bce859435a18
JuanSR <jfrancisco1717@gmail.com>
2020-05-19 12:29:30 -0400

    Fix changelist_view comments admin panel

--- comments/admin.py
+++ comments/admin.py
@@ -10,23 +10,19 @@ class CommentAdmin(admin.ModelAdmin):
     list_filter = ('user', 'post_id')
 
     def changelist_view(self, request, extra_context=None):
-        chart_data = self.chart_data()
-        as_json = json.dumps(list(chart_data), cls=DjangoJSONEncoder)
+        posts_comments =Comment.objects \
+            .values("post__title") \
+            .annotate(y=Count("id")) \
+            .order_by("-post")
+        posts_comments_json = json.dumps(list(posts_comments), cls=DjangoJSONEncoder)
 
-        user_comments = Comment.objects.values("user__username") \
+        user_comments = Comment.objects \
+            .values("user__username") \
             .annotate(y=Count("id")) \
             .order_by("-user__username")
         user_comments_json = json.dumps(list(user_comments), cls=DjangoJSONEncoder)
 
         extra_context = extra_context or \
-            {"chart_data": as_json, "user_comments": user_comments_json}
+            {"posts_comments": posts_comments_json, "user_comments": user_comments_json}
 
         return super().changelist_view(request, extra_context=extra_context)
-
-    def chart_data(self):
-        return (
-            Comment.objects
-                .values("post__title")
-                .annotate(y=Count("id"))
-                .order_by("-post")
-        )
