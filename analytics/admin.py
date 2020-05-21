c771511c3121f8b2ef370a047c114fccd7f8a50e
JuanSR <jfrancisco1717@gmail.com>
2020-05-21 08:21:22 -0400

    Add Analytics chart for visited posts

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,6 +1,20 @@
+import json
 from django.contrib import admin
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import Count
 from analytics.models import ObjectViewed
 
 @admin.register(ObjectViewed)
 class ObjectViewedAdmin(admin.ModelAdmin):
     list_display = ('content_object', 'content_type', 'viewed_on', 'user', 'ip_address')
+
+    def changelist_view(self, request, extra_context=None):
+        # Aggregate number of post visited per user
+        user_num_post = ObjectViewed.objects \
+            .values("user__username") \
+            .annotate(y=Count("object_id")) \
+            .order_by("user__username")
+        as_json = json.dumps(list(user_num_post), cls=DjangoJSONEncoder)
+        extra_context = extra_context or {"user_num_post": as_json}
+
+        return super().changelist_view(request, extra_context=extra_context)
